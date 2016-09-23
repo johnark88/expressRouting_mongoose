b1d6235ff28a63b9c2a3ce712500d37d37a5e26c
John Arkema <john.arkema@gmail.com>
2016-09-23 10:07:05 -0500

    express routers

---
+++ routers/userRouter.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+
+//model
+var User = require('../models/user');
+
+
+router.get('/all', function(req, res) {
+
+  User.find({}, function(err, userResults) {
+    if(err){
+      console.log('error occurred:', err);
+      res.sendStatus(500);
+    }else{
+      res.send(userResults);
+    }
+  });
+});
+
+router.post('/create', function(req, res) {
+  console.log('hit the post');
+  console.log('request body', req.body);
+
+  var sentData = req.body;
+
+  var newUser = new User({
+    name: sentData.firstName, //Millie
+    username: sentData.userName, //millie
+    admin: sentData.admin
+  });
+
+  newUser.save(function(err) {
+    if(err){
+      //failed
+      console.log('error occurred:', err);
+      res.sendStatus(500);
+    }else{
+      console.log('New user saved successfully!');
+      res.sendStatus(201);
+    }
+  });
+});
+
+
+module.exports = router;
